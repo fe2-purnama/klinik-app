c916f9c41973651bf06937c56a41ddbafb38dcb0
asfari7 <muhammadasnafi4@gmail.com>
2024-05-30 09:09:40 +0700

    megganti input fomr yang sudah dibuat dengan component input form

--- klinik-app/src/components/FormReservasi.jsx
+++ klinik-app/src/components/FormReservasi.jsx
@@ -1,136 +1,24 @@
 import React from "react";
+import InputForm from "./../components/InputForm";
 
 function FormReservasi() {
   return (
     <form className="my-7 p-7 border-2 border-slate-200 rounded-lg">
       <h1 className="text-2xl font-semibold my-4">Data Diri</h1>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 gap-x-20">
-        <div className="input-group">
-          <label className="font-medium">Nama Lengkap</label>
-          <input
-            type="text"
-            className="form-control
-              block
-              w-full
-              mt-4
-              px-3
-              py-1.5
-              text-base
-              font-normal
-              text-gray-700
-              bg-white bg-clip-padding
-              border border-solid border-gray-300
-              rounded
-              transition
-              ease-in-out
-              m-0
-              focus:text-gray-700 focus:bg-white focus:border-sky-200 focus:outline-none"
-            id="exampleFormControlInput1"
-            placeholder="Masukkan Nama Lengkap"
-            required
-          />
-        </div>
-        <div className="input-group">
-          <label className="font-medium">Alamat</label>
-          <input
-            type="text"
-            className="form-control
-              block
-              w-full
-              mt-4
-              px-3
-              py-1.5
-              text-base
-              font-normal
-              text-gray-700
-              bg-white bg-clip-padding
-              border border-solid border-gray-300
-              rounded
-              transition
-              ease-in-out
-              m-0
-              focus:text-gray-700 focus:bg-white focus:border-sky-200 focus:outline-none"
-            id="exampleFormControlInput1"
-            placeholder="Masukkan Alamat Lengkap"
-            required
-          />
-        </div>
-        <div className="input-group">
-          <label className="font-medium">NIK</label>
-          <input
-            type="text"
-            className="form-control
-              block
-              w-full
-              mt-4
-              px-3
-              py-1.5
-              text-base
-              font-normal
-              text-gray-700
-              bg-white bg-clip-padding
-              border border-solid border-gray-300
-              rounded
-              transition
-              ease-in-out
-              m-0
-              focus:text-gray-700 focus:bg-white focus:border-sky-200 focus:outline-none"
-            id="exampleFormControlInput1"
-            placeholder="Masukkan NIK"
-            required
-          />
-        </div>
-        <div className="input-group">
-          <label className="font-medium">Tanggal Reservasi</label>
-          <input
-            type="date"
-            className="form-control
-              block
-              w-full
-              mt-4
-              px-3
-              py-1.5
-              text-base
-              font-normal
-              text-gray-700
-              bg-white bg-clip-padding
-              border border-solid border-gray-300
-              rounded
-              transition
-              ease-in-out
-              m-0
-              focus:text-gray-700 focus:bg-white focus:border-sky-200 focus:outline-none"
-            id="exampleFormControlInput1"
-            placeholder="Masukkan Nama Dokter"
-            required
-            min={new Date().toISOString().split("T")[0]}
-          />
-        </div>
-        <div className="input-group">
-          <label className="font-medium">Tempat, tanggal lahir</label>
-          <input
-            type="text"
-            className="form-control
-              block
-              w-full
-              mt-4
-              px-3
-              py-1.5
-              text-base
-              font-normal
-              text-gray-700
-              bg-white bg-clip-padding
-              border border-solid border-gray-300
-              rounded
-              transition
-              ease-in-out
-              m-0
-              focus:text-gray-700 focus:bg-white focus:border-sky-200 focus:outline-none"
-            id="exampleFormControlInput1"
-            placeholder="Masukkan Tempat, Tanggal Lahir"
-            required
-          />
-        </div>
+        <InputForm
+          label="Nama Lengkap"
+          type="text"
+          placeholder="Masukkan nama lengkap"
+        />
+        <InputForm label="Alamat" type="text" placeholder="Masukkan alamat" />
+        <InputForm label="NIK" type="text" placeholder="Masukkan NIK" />
+        <InputForm label="Tanggal Reservasi" type="date" />
+        <InputForm
+          label="Tempat, tanggal lahir"
+          type="text"
+          placeholder="Masukkan tempat, tanggal lahir"
+        />
         <div
           className="input-group 
               row-span-2"
@@ -186,31 +74,7 @@ function FormReservasi() {
             <option>Perempuan</option>
           </select>
         </div>
-        <div className="input-group">
-          <label className="font-medium">No Hp</label>
-          <input
-            type="number"
-            className="form-control
-              block
-              w-full
-              mt-4
-              px-3
-              py-1.5
-              text-base
-              font-normal
-              text-gray-700
-              bg-white bg-clip-padding
-              border border-solid border-gray-300
-              rounded
-              transition
-              ease-in-out
-              m-0
-              focus:text-gray-700 focus:bg-white focus:border-sky-200 focus:outline-none"
-            id="exampleFormControlInput1"
-            placeholder="Masukkan Nomo HP"
-            required
-          />
-        </div>
+        <InputForm label="No HP" type="number" placeholder="Masukkan No Hp" />
         <button className="bg-sky-500 hover:bg-sky-600 text-white font-bold py-2 px-4 rounded h-fit w-full place-self-end">
           Reservasi Sekarang
         </button>
