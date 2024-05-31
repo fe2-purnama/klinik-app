4b265a49c41f2d74f45fa140245a884b670f482f
iqbalfatoni10 <mohiqbalfatoni10@gmail.com>
2024-05-31 09:10:26 +0700

    memperbaiki konflik

--- klinik-app/tailwind.config.js
+++ klinik-app/tailwind.config.js
@@ -1,19 +1,10 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 
-const flowbite = require("flowbite-react/tailwind");
+const flowbite = require('flowbite-react/tailwind');
 
 export default {
-<<<<<<< HEAD
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/flowbite-react/lib/esm/**/*.js'],
-=======
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite-react/lib/esm/**/*.js",
-    flowbite.content(),
-  ],
->>>>>>> d1d32293294956938c6838ed38f539defbcd1907
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/flowbite-react/lib/esm/**/*.js', flowbite.content()],
   theme: {
     extend: {},
     container: {
@@ -21,14 +12,5 @@ export default {
       padding: '1rem',
     },
   },
-<<<<<<< HEAD
-  plugins: [require('flowbite/plugin')],
+  plugins: [require('flowbite/plugin'), flowbite.plugin()],
 };
-=======
-  plugins: [
-    require('flowbite/plugin'),
-    flowbite.plugin(),
-  ],
-}
-
->>>>>>> d1d32293294956938c6838ed38f539defbcd1907
