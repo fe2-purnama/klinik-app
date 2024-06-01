d2b5d01743cb11cd3c5ec5307b082f34bdc5d646
RizqiZamzamiJamil <rizqizamzamij@gmail.com>
2024-06-01 12:45:58 +0700

    update dashboard dokter

--- klinik-app/src/components/DashboardDokter/TableDokter.jsx
+++ klinik-app/src/components/DashboardDokter/TableDokter.jsx
@@ -5,17 +5,17 @@ const Table = ({ headers, children }) => {
         <div className="flex flex-col mt-8">
             <div className="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
                 <div className="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
-                    <table className="min-w-full">
-                        <thead>
+                    <table className="min-w-full divide-y divide-gray-200">
+                        <thead className="bg-gray-50">
                             <tr>
                                 {headers.map((header, index) => (
-                                    <th key={index} className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                                    <th key={index} className="px-2 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 md:px-6">
                                         {header}
                                     </th>
                                 ))}
                             </tr>
                         </thead>
-                        <tbody className="bg-white">
+                        <tbody className="bg-white divide-y divide-gray-200">
                             {children}
                         </tbody>
                     </table>
