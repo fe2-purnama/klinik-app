6e7145495c70333658719083dc96031fca06b39f
iqbalfatoni10 <mohiqbalfatoni10@gmail.com>
2024-06-02 14:52:32 +0700

    memperbaiki form

--- klinik-app/src/components/TableDokter/TableDokter.jsx
+++ klinik-app/src/components/TableDokter/TableDokter.jsx
@@ -1,120 +1,113 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeadCell,
-  TableRow,
-} from "flowbite-react";
+import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from 'flowbite-react';
 
 const dokter = [
   {
-    str: "00000007878011999",
-    dokter: "Dr. Andi Wijaya",
-    jenis_kelamin: "Laki-laki",
-    spesialis: "Kardiologi",
-    no_hp: "083423432231",
-    pengalaman: "4",
+    str: '00000007878011999',
+    dokter: 'Dr. Andi Wijaya',
+    jenis_kelamin: 'Laki-laki',
+    spesialis: 'Kardiologi',
+    no_hp: '083423432231',
+    pengalaman: '4',
   },
   {
-    str: "00000007878011999",
-    dokter: "Dr. Andi Wijaya",
-    jenis_kelamin: "Laki-laki",
-    spesialis: "Kardiologi",
-    no_hp: "083423432231",
-    pengalaman: "4",
+    str: '00000007878011999',
+    dokter: 'Dr. Andi Wijaya',
+    jenis_kelamin: 'Laki-laki',
+    spesialis: 'Kardiologi',
+    no_hp: '083423432231',
+    pengalaman: '4',
   },
   {
-    str: "00000007878011999",
-    dokter: "Dr. Andi Wijaya",
-    jenis_kelamin: "Laki-laki",
-    spesialis: "Kardiologi",
-    no_hp: "083423432231",
-    pengalaman: "4",
+    str: '00000007878011999',
+    dokter: 'Dr. Andi Wijaya',
+    jenis_kelamin: 'Laki-laki',
+    spesialis: 'Kardiologi',
+    no_hp: '083423432231',
+    pengalaman: '4',
   },
   {
-    str: "00000007878011999",
-    dokter: "Dr. Andi Wijaya",
-    jenis_kelamin: "Laki-laki",
-    spesialis: "Kardiologi",
-    no_hp: "083423432231",
-    pengalaman: "4",
+    str: '00000007878011999',
+    dokter: 'Dr. Andi Wijaya',
+    jenis_kelamin: 'Laki-laki',
+    spesialis: 'Kardiologi',
+    no_hp: '083423432231',
+    pengalaman: '4',
   },
   {
-    str: "00000007878011999",
-    dokter: "Dr. Andi Wijaya",
-    jenis_kelamin: "Laki-laki",
-    spesialis: "Kardiologi",
-    no_hp: "083423432231",
-    pengalaman: "4",
+    str: '00000007878011999',
+    dokter: 'Dr. Andi Wijaya',
+    jenis_kelamin: 'Laki-laki',
+    spesialis: 'Kardiologi',
+    no_hp: '083423432231',
+    pengalaman: '4',
   },
   {
-    str: "00000007878011999",
-    dokter: "Dr. Andi Wijaya",
-    jenis_kelamin: "Laki-laki",
-    spesialis: "Kardiologi",
-    no_hp: "083423432231",
-    pengalaman: "4",
+    str: '00000007878011999',
+    dokter: 'Dr. Andi Wijaya',
+    jenis_kelamin: 'Laki-laki',
+    spesialis: 'Kardiologi',
+    no_hp: '083423432231',
+    pengalaman: '4',
   },
   {
-    str: "00000007878011999",
-    dokter: "Dr. Andi Wijaya",
-    jenis_kelamin: "Laki-laki",
-    spesialis: "Kardiologi",
-    no_hp: "083423432231",
-    pengalaman: "4",
+    str: '00000007878011999',
+    dokter: 'Dr. Andi Wijaya',
+    jenis_kelamin: 'Laki-laki',
+    spesialis: 'Kardiologi',
+    no_hp: '083423432231',
+    pengalaman: '4',
   },
   {
-    str: "00000007878011999",
-    dokter: "Dr. Andi Wijaya",
-    jenis_kelamin: "Laki-laki",
-    spesialis: "Kardiologi",
-    no_hp: "083423432231",
-    pengalaman: "4",
+    str: '00000007878011999',
+    dokter: 'Dr. Andi Wijaya',
+    jenis_kelamin: 'Laki-laki',
+    spesialis: 'Kardiologi',
+    no_hp: '083423432231',
+    pengalaman: '4',
   },
   {
-    str: "00000007878011999",
-    dokter: "Dr. Andi Wijaya",
-    jenis_kelamin: "Laki-laki",
-    spesialis: "Kardiologi",
-    no_hp: "083423432231",
-    pengalaman: "4",
+    str: '00000007878011999',
+    dokter: 'Dr. Andi Wijaya',
+    jenis_kelamin: 'Laki-laki',
+    spesialis: 'Kardiologi',
+    no_hp: '083423432231',
+    pengalaman: '4',
   },
   {
-    str: "00000007878011999",
-    dokter: "Dr. Andi Wijaya",
-    jenis_kelamin: "Laki-laki",
-    spesialis: "Kardiologi",
-    no_hp: "083423432231",
-    pengalaman: "4",
+    str: '00000007878011999',
+    dokter: 'Dr. Andi Wijaya',
+    jenis_kelamin: 'Laki-laki',
+    spesialis: 'Kardiologi',
+    no_hp: '083423432231',
+    pengalaman: '4',
   },
   {
-    str: "00000007878011999",
-    dokter: "Dr. Andi Wijaya",
-    jenis_kelamin: "Laki-laki",
-    spesialis: "Kardiologi",
-    no_hp: "083423432231",
-    pengalaman: "4",
+    str: '00000007878011999',
+    dokter: 'Dr. Andi Wijaya',
+    jenis_kelamin: 'Laki-laki',
+    spesialis: 'Kardiologi',
+    no_hp: '083423432231',
+    pengalaman: '4',
   },
   {
-    str: "00000007878011999",
-    dokter: "Dr. Andi Wijaya",
-    jenis_kelamin: "Laki-laki",
-    spesialis: "Kardiologi",
-    no_hp: "083423432231",
-    pengalaman: "4",
+    str: '00000007878011999',
+    dokter: 'Dr. Andi Wijaya',
+    jenis_kelamin: 'Laki-laki',
+    spesialis: 'Kardiologi',
+    no_hp: '083423432231',
+    pengalaman: '4',
   },
   {
-    str: "00000007878011999",
-    dokter: "Dr. Andi Wijaya",
-    jenis_kelamin: "Laki-laki",
-    spesialis: "Kardiologi",
-    no_hp: "083423432231",
-    pengalaman: "4",
+    str: '00000007878011999',
+    dokter: 'Dr. Andi Wijaya',
+    jenis_kelamin: 'Laki-laki',
+    spesialis: 'Kardiologi',
+    no_hp: '083423432231',
+    pengalaman: '4',
   },
 ];
 
@@ -136,18 +129,16 @@ const TableDokter = () => {
       <TableBody className="bg-white">
         {dokter.map((dokter, index) => (
           <TableRow key={index}>
-            <TableCell className="font-medium text-gray-900 text-center">
-              {index + 1}
-            </TableCell>
+            <TableCell className="font-medium text-gray-900 text-center">{index + 1}</TableCell>
             <TableCell>{dokter.str}</TableCell>
             <TableCell>{dokter.dokter}</TableCell>
             <TableCell>{dokter.jenis_kelamin}</TableCell>
             <TableCell>{dokter.spesialis}</TableCell>
             <TableCell>{dokter.no_hp}</TableCell>
-            <TableCell>{dokter.pengalaman}</TableCell>
-            <TableCell>
+            <TableCell className="text-center">{dokter.pengalaman}</TableCell>
+            <TableCell className="">
               <Link to="/admin/edit">
-                <button className="bg-sky-500 px-2 py-1 rounded mr-2 hover:bg-sky-600 shadow-none">
+                <button className="my-2 bg-sky-500 px-2 py-1 rounded mr-2 hover:bg-sky-600 shadow-none">
                   <i className="text-white fas fa-pen"></i>
                 </button>
               </Link>
