e5538fe34ae76bd28c18d0341a594efdd5c72d39
iqbalfatoni10 <mohiqbalfatoni10@gmail.com>
2024-06-06 20:41:05 +0700

    memperbaiki responsive dashboard admin

--- klinik-app/src/components/SidebarAdmin/Sidebar.jsx
+++ klinik-app/src/components/SidebarAdmin/Sidebar.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import Logo from "../../assets/logo.png";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/logo.png';
 
 const Sidebar = (props) => {
   const { Show } = props;
@@ -17,64 +17,38 @@ const Sidebar = (props) => {
   }, [Show]);
 
   return (
-    <aside
-      className={`h-screen px-3 py-4 bg-white flex-col border-e-2 lg:flex z-20 ${
-        isShow ? "absolute" : "hidden"
-      }`}
-    >
-      <ul className="flex flex-col justify-between h-full space-y-2 font-medium flex-1">
+    <aside className={`fixed inset-y-0 left-0 z-20 w-64 bg-white border-r-2 lg:relative lg:w-auto lg:flex lg:border-none transition-transform duration-300 ${isShow ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}`}>
+      <ul className="flex flex-col justify-between h-full space-y-2 font-medium">
         <li>
-          <div className="logo flex items-center mb-8">
+          <div className="logo flex items-center mb-8 p-4">
             <img src={Logo} alt="Klinik Prima" className="h-12" />
-            <h1 className="sm:text-sm md:text-lg lg:text-xl xl:text-2xl ml-2">
-              <span className="text-[color:var(--primary)] font-semibold">
-                Klinik
-              </span>
-              <span className="text-[color:var(--other)] font-semibold">
-                Prima
-              </span>
+            <h1 className="ml-2 text-xl font-semibold text-[color:var(--primary)]">
+              <span>Klinik</span>
+              <span className="text-[color:var(--other)]">Prima</span>
             </h1>
           </div>
-          <button
-            className={`absolute top-7 right-4 shadow-none bg-white ${
-              isShow ? "block" : "hidden"
-            }`}
-            onClick={handleClick}
-          >
+          <button className={`absolute top-4 right-4 shadow-none bg-white ${isShow ? 'block' : 'hidden'} lg:hidden`} onClick={handleClick}>
             <i className="fal fa-times fa-xl"></i>
           </button>
         </li>
-        <div className="flex-1">
+        <div className="flex-1 px-4">
           <li className="mb-4">
-            <Link
-              className="flex items-center p-2 px-4 text-gray-900 rounded-lg hover:bg-blue-100"
-              to="/admin"
-            >
+            <Link className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-blue-100" to="/admin">
               <i className="far fa-user-md fa-xl"></i>
-              <span className="flex-1 ms-3 whitespace-nowrap">
-                Manage Akun Dokter
-              </span>
+              <span className="flex-1 ml-3 whitespace-nowrap">Manage Akun Dokter</span>
             </Link>
           </li>
           <li>
-            <Link
-              className="flex items-center p-2 px-4 text-gray-900 rounded-lg hover:bg-blue-100"
-              to="/admin/add"
-            >
+            <Link className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-blue-100" to="/admin/add">
               <i className="far fa-user-plus fa-xl"></i>
-              <span className="flex-1 ms-3 whitespace-nowrap">
-                Tambah Akun Dokter
-              </span>
+              <span className="flex-1 ml-3 whitespace-nowrap">Tambah Akun Dokter</span>
             </Link>
           </li>
         </div>
-        <li className="mt-auto">
-          <Link
-            className="flex items-center p-2 px-4 text-gray-900 rounded-lg hover:bg-blue-100"
-            to="/Login"
-          >
+        <li className="mt-auto p-4">
+          <Link className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-blue-100" to="/Login">
             <i className="far fa-sign-out fa-2xl"></i>
-            <span className="flex-1 ms-3 whitespace-nowrap">Logout</span>
+            <span className="flex-1 ml-3 whitespace-nowrap">Logout</span>
           </Link>
         </li>
       </ul>
