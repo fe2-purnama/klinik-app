8913e1be1fb9d3bd9f33f5708e1647c64662941a
asfari7 <muhammadasnafi4@gmail.com>
2024-06-11 14:57:31 +0700

    migrasi

--- klinik-app/server/README.md
+++
@@ -1,6 +0,0 @@
-Express js & REST API 
-# how to use 
-1. git clone this repo 
-2. npm i 
-# how to run 
-npm start
--- klinik-app/server/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (id, role) => {
-    return jwt.sign({ id, role }, 'secretkey', {
-        expiresIn: '1h'
-    });
-};
-
-module.exports = generateToken;
