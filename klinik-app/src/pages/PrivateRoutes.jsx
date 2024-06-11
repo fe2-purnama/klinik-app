e8fc06cf42c761b59c8ae6292e341e5744efd573
asfari7 <muhammadasnafi4@gmail.com>
2024-06-11 15:24:34 +0700

    add validasi token untuk route dashboard admin

--- klinik-app/src/pages/PrivateRoutes.jsx
+++ klinik-app/src/pages/PrivateRoutes.jsx
@@ -2,15 +2,10 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ element, ...rest }) => {
-  const isAuthenticated = true;
-  const isAdmin = true;
+  const token = localStorage.getItem("token");
 
-  if (!isAuthenticated) {
+  if (!token) {
     return <Navigate to="/Login" />;
-  }
-
-  if (!isAdmin) {
-    return <Navigate to="/" />;
   }
 
   return element;
