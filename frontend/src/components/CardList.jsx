63b2e905197c6258d91580499afd735c930fedf3
asfari7 <muhammadasnafi4@gmail.com>
2024-06-13 08:36:24 +0700

    add object fit cover dan validasi formreservasi ketika belum login

--- frontend/src/components/CardList.jsx
+++ frontend/src/components/CardList.jsx
@@ -9,7 +9,7 @@ function CardList(props) {
       <div className="rounded-lg overflow-hidden relative">
         <img
           src={img || "https://placehold.co/600x600?text=Foto+Dokter"}
-          className="rounded-lg aspect-square"
+          className="rounded-lg aspect-square object-cover"
           alt="Foto Dokter"
         />
 
