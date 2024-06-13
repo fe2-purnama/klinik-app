b958d289e71c6b1a8a0b643ff1a4949f7cfdded2
asfari7 <muhammadasnafi4@gmail.com>
2024-06-13 17:24:32 +0700

    fix layout admin

--- frontend/src/pages/DashboardAdmin/AdminLayout.jsx
+++ frontend/src/pages/DashboardAdmin/AdminLayout.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import Sidebar from '../../components/SidebarAdmin/Sidebar';
-import { Outlet } from 'react-router-dom';
-import HeaderAdmin from '../../components/HeaderAdmin/HeaderAdmin';
+import React, { useState } from "react";
+import Sidebar from "../../components/SidebarAdmin/Sidebar";
+import { Outlet } from "react-router-dom";
+import HeaderAdmin from "../../components/HeaderAdmin/HeaderAdmin";
 
 function AdminLayout() {
   const [isShow, setIsShow] = useState(false);
@@ -10,11 +10,14 @@ function AdminLayout() {
     setIsShow(!isShow);
   };
   return (
-    <section className="sm:h-screen w-full flex flex-row bg-slate-200">
+    <section className="h-full sm:h-screen w-full flex flex-row bg-slate-200">
       <Sidebar Show={isShow} />
       <section className="h-full w-full">
         <div className="flex flex-row items-center px-5 py-2 gap-4 border-b-2 sticky top-0 bg-white z-10">
-          <a className="bg-white p-2 rounded-lg shadow-none cursor-pointer lg:hidden" onClick={handleClick}>
+          <a
+            className="bg-white p-2 rounded-lg shadow-none cursor-pointer lg:hidden"
+            onClick={handleClick}
+          >
             <i className="fas fa-bars"></i>
           </a>
           <HeaderAdmin />
