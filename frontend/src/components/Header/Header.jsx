c8f6aead6c293963a1276eb299c03f97a3879e9b
iqbalfatoni10 <mohiqbalfatoni10@gmail.com>
2024-06-14 12:01:15 +0700

    update header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,160 +1,213 @@
 import { useEffect, useState } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import Logo from '../../assets/logo.png';
 import './Style.css';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+  const [isHome, setIsHome] = useState(location.pathname === '/');
+  const [activeSection, setActiveSection] = useState('home');
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
 
-    const [isOpen, setIsOpen] = useState(false);
-    const location = useLocation();
-    const [isHome, setIsHome] = useState(location.pathname === '/');
-    const [activeSection, setActiveSection] = useState('home');
+  useEffect(() => {
+    setIsHome(location.pathname === '/');
+  }, [location]);
 
-    useEffect(() => {
-        setIsHome(location.pathname === '/');
-    }, [location]);
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = ['home', 'layanan', 'dokter', 'tentang', 'faqs', 'hubungi'];
+      const halfWindowHeight = window.innerHeight / 2;
 
-    useEffect(() => {
-        const handleScroll = () => {
-        const sections = ['home', 'layanan', 'dokter', 'tentang', 'faqs', 'hubungi'];
-        const halfWindowHeight = window.innerHeight / 2;
-
-        for (let section of sections) {
-            const element = document.getElementById(section);
-            if (element) {
-            const rect = element.getBoundingClientRect();
-            if (rect.top < halfWindowHeight && rect.bottom > halfWindowHeight) {
-                setActiveSection(section);
-                break;
-            }
-            }
+      for (let section of sections) {
+        const element = document.getElementById(section);
+        if (element) {
+          const rect = element.getBoundingClientRect();
+          if (rect.top < halfWindowHeight && rect.bottom > halfWindowHeight) {
+            setActiveSection(section);
+            break;
+          }
         }
-        };
+      }
+    };
 
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-        window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
-    return (
-        <>
-        <nav className="sticky top-0 w-full">
-            <div className="mx-4 md:mx-16 flex justify-between items-center py-0">
-            {/* Logo */}
-            <div className="logo flex items-center">
-                <img src={Logo} alt="Klinik Prima" className="h-12" />
-                <h1 className="sm:text:sm md:text-lg lg:text-xl xl:text-2xl font-bold ml-2">
-                <span className="text-[color:var(--primary)]">Klinik </span>
-                <span className="text-[color:var(--other)]">Prima</span>
-                </h1>
-            </div>
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+    navigate('/Login');
+  };
 
-            {/* Menu */}
-            <ul className="nav-links hidden lg:flex space-x-8">
-                <li className={activeSection === 'home' ? 'active' : ''}>
-                {isHome ? (
-                    <AnchorLink className="anchor-link" offset={80} href="#home">
-                    Beranda
-                    </AnchorLink>
-                ) : (
-                    <Link to="/">Beranda</Link>
-                )}
-                </li>
-                <li className={activeSection === 'layanan' ? 'active' : ''}>
-                <AnchorLink className="anchor-link" offset={120} href="#layanan">
-                    Layanan
+  const handleLogoutClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleConfirmLogout = () => {
+    handleLogout();
+    handleModalClose();
+  };
+
+  return (
+    <>
+      <nav className="sticky top-0 w-full">
+        <div className="mx-4 md:mx-16 flex justify-between items-center py-0">
+          {/* Logo */}
+          <div className="logo flex items-center">
+            <img src={Logo} alt="Klinik Prima" className="h-12" />
+            <h1 className="sm:text:sm md:text-lg lg:text-xl xl:text-2xl font-bold ml-2">
+              <span className="text-[color:var(--primary)]">Klinik </span>
+              <span className="text-[color:var(--other)]">Prima</span>
+            </h1>
+          </div>
+
+          {/* Menu */}
+          <ul className="nav-links hidden lg:flex space-x-8">
+            <li className={activeSection === 'home' ? 'active' : ''}>
+              {isHome ? (
+                <AnchorLink className="anchor-link" offset={80} href="#home">
+                  Beranda
                 </AnchorLink>
-                </li>
-                <li className={activeSection === 'dokter' ? 'active' : ''}>
-                <AnchorLink className="anchor-link" offset={120} href="#dokter">
-                    Dokter
-                </AnchorLink>
-                </li>
-                <li className={activeSection === 'tentang' ? 'active' : ''}>
-                <AnchorLink className="anchor-link" offset={120} href="#tentang">
-                    Tentang
-                </AnchorLink>
-                </li>
-                <li className={activeSection === 'faqs' ? 'active' : ''}>
-                <AnchorLink className="anchor-link" offset={120} href="#faqs">
-                    FAQs
-                </AnchorLink>
-                </li>
-                <li className={activeSection === 'hubungi' ? 'active' : ''}>
-                <AnchorLink className="anchor-link" offset={120} href="#hubungi">
-                    Hubungi
-                </AnchorLink>
-                </li>
-                <li>
+              ) : (
+                <Link to="/">Beranda</Link>
+              )}
+            </li>
+            <li className={activeSection === 'layanan' ? 'active' : ''}>
+              <AnchorLink className="anchor-link" offset={120} href="#layanan">
+                Layanan
+              </AnchorLink>
+            </li>
+            <li className={activeSection === 'dokter' ? 'active' : ''}>
+              <AnchorLink className="anchor-link" offset={120} href="#dokter">
+                Dokter
+              </AnchorLink>
+            </li>
+            <li className={activeSection === 'tentang' ? 'active' : ''}>
+              <AnchorLink className="anchor-link" offset={120} href="#tentang">
+                Tentang
+              </AnchorLink>
+            </li>
+            <li className={activeSection === 'faqs' ? 'active' : ''}>
+              <AnchorLink className="anchor-link" offset={120} href="#faqs">
+                FAQs
+              </AnchorLink>
+            </li>
+            <li className={activeSection === 'hubungi' ? 'active' : ''}>
+              <AnchorLink className="anchor-link" offset={120} href="#hubungi">
+                Hubungi
+              </AnchorLink>
+            </li>
+            <li>
+              {isLoggedIn ? (
+                <button className="btn-logout" onClick={handleLogoutClick}>
+                  Keluar
+                </button>
+              ) : (
                 <Link className="btn-login" to="/Login">
-                    Masuk
+                  Masuk
                 </Link>
-                </li>
-            </ul>
+              )}
+            </li>
+          </ul>
 
-            <button className="lg:hidden flex items-center" onClick={() => setIsOpen(!isOpen)}>
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-                </svg>
-            </button>
-            </div>
-        </nav>
-
-        {/* Sidebar */}
-        <div className={`sidebar fixed top-0 right-0 w-64 h-full bg-[color:var(--secondary)] shadow-lg transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out z-20`}>
-            <button className="absolute top-4 right-4 text-gray-600 hover:text-red-500 transition-colors duration-200" onClick={() => setIsOpen(false)}>
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-                </svg>
-            </button>
-            <ul className="flex flex-col items-start mt-20 space-y-3">
-                <li className={`w-56 py-2 px-6 mx-3 rounded-xl ${activeSection === 'home' ? 'bg-[color:var(--primary)] text-white' : 'hover:bg-[color:var(--tertiary)] transition-colors duration-200'}`}>
-                {isHome ? (
-                    <AnchorLink className="anchor-link block w-full h-full" offset={80} href="#home">
-                    Beranda
-                    </AnchorLink>
-                ) : (
-                    <Link className="block w-full h-full" to="/">Home</Link>
-                )}
-                </li>
-                <li className={`w-56 py-2 px-6 mx-3 rounded-xl ${activeSection === 'layanan' ? 'bg-[color:var(--primary)] text-white' : 'hover:bg-[color:var(--tertiary)] transition-colors duration-200'}`}>
-                    <AnchorLink className="anchor-link block w-full h-full" offset={50} href="#layanan">
-                        Layanan
-                    </AnchorLink>
-                </li>
-                <li className={`w-56 py-2 px-6 mx-3 rounded-xl ${activeSection === 'dokter' ? 'bg-[color:var(--primary)] text-white' : 'hover:bg-[color:var(--tertiary)] transition-colors duration-200'}`}>
-                    <AnchorLink className="anchor-link block w-full h-full" offset={100} href="#dokter">
-                        Dokter
-                    </AnchorLink>
-                </li>
-                <li className={`w-56 py-2 px-6 mx-3 rounded-xl ${activeSection === 'tentang' ? 'bg-[color:var(--primary)] text-white' : 'hover:bg-[color:var(--tertiary)] transition-colors duration-200'}`}>
-                    <AnchorLink className="anchor-link block w-full h-full" offset={100} href="#tentang">
-                        Tentang
-                    </AnchorLink>
-                </li>
-                <li className={`w-56 py-2 px-6 mx-3 rounded-xl ${activeSection === 'faqs' ? 'bg-[color:var(--primary)] text-white' : 'hover:bg-[color:var(--tertiary)] transition-colors duration-200'}`}>
-                    <AnchorLink className="anchor-link block w-full h-full" offset={100} href="#faqs">
-                        FAQs
-                    </AnchorLink>
-                </li>
-                <li className={`w-56 py-2 px-6 mx-3 rounded-xl ${activeSection === 'hubungi' ? 'bg-[color:var(--primary)] text-white' : 'hover:bg-[color:var(--tertiary)] transition-colors duration-200'}`}>
-                    <AnchorLink className="anchor-link block w-full h-full" offset={100} href="#hubungi">
-                        Hubungi
-                    </AnchorLink>
-                </li>
-                <li className="w-56 pt-16 mx-3">
-                    <Link className="btn-login bg-[color:var(--secondary)] text-[color:var(--primary)] block w-full h-full hover:bg-[color:var(--primary)] hover:text-white" to="/Login" onClick={() => setIsOpen(false)}>
-                        Masuk
-                    </Link>
-                </li>
-            </ul>
+          <button className="lg:hidden flex items-center" onClick={() => setIsOpen(!isOpen)}>
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+            </svg>
+          </button>
         </div>
+      </nav>
 
-        <Outlet />
-        </>
-    );
+      {/* Sidebar */}
+      <div className={`sidebar fixed top-0 right-0 w-64 h-full bg-[color:var(--secondary)] shadow-lg transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out z-20`}>
+        <button className="absolute top-4 right-4 text-gray-600 hover:text-red-500 transition-colors duration-200" onClick={() => setIsOpen(false)}>
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+          </svg>
+        </button>
+        <ul className="flex flex-col items-start mt-20 space-y-3">
+          <li className={`w-56 py-2 px-6 mx-3 rounded-xl ${activeSection === 'home' ? 'bg-[color:var(--primary)] text-white' : 'hover:bg-[color:var(--tertiary)] transition-colors duration-200'}`}>
+            {isHome ? (
+              <AnchorLink className="anchor-link block w-full h-full" offset={80} href="#home">
+                Beranda
+              </AnchorLink>
+            ) : (
+              <Link className="block w-full h-full" to="/">
+                Home
+              </Link>
+            )}
+          </li>
+          <li className={`w-56 py-2 px-6 mx-3 rounded-xl ${activeSection === 'layanan' ? 'bg-[color:var(--primary)] text-white' : 'hover:bg-[color:var(--tertiary)] transition-colors duration-200'}`}>
+            <AnchorLink className="anchor-link block w-full h-full" offset={50} href="#layanan">
+              Layanan
+            </AnchorLink>
+          </li>
+          <li className={`w-56 py-2 px-6 mx-3 rounded-xl ${activeSection === 'dokter' ? 'bg-[color:var(--primary)] text-white' : 'hover:bg-[color:var(--tertiary)] transition-colors duration-200'}`}>
+            <AnchorLink className="anchor-link block w-full h-full" offset={100} href="#dokter">
+              Dokter
+            </AnchorLink>
+          </li>
+          <li className={`w-56 py-2 px-6 mx-3 rounded-xl ${activeSection === 'tentang' ? 'bg-[color:var(--primary)] text-white' : 'hover:bg-[color:var(--tertiary)] transition-colors duration-200'}`}>
+            <AnchorLink className="anchor-link block w-full h-full" offset={100} href="#tentang">
+              Tentang
+            </AnchorLink>
+          </li>
+          <li className={`w-56 py-2 px-6 mx-3 rounded-xl ${activeSection === 'faqs' ? 'bg-[color:var(--primary)] text-white' : 'hover:bg-[color:var(--tertiary)] transition-colors duration-200'}`}>
+            <AnchorLink className="anchor-link block w-full h-full" offset={100} href="#faqs">
+              FAQs
+            </AnchorLink>
+          </li>
+          <li className={`w-56 py-2 px-6 mx-3 rounded-xl ${activeSection === 'hubungi' ? 'bg-[color:var(--primary)] text-white' : 'hover:bg-[color:var(--tertiary)] transition-colors duration-200'}`}>
+            <AnchorLink className="anchor-link block w-full h-full" offset={100} href="#hubungi">
+              Hubungi
+            </AnchorLink>
+          </li>
+          <li className="w-56 pt-16 mx-3">
+            {isLoggedIn ? (
+              <button className="btn-logout bg-[color:var(--secondary)] text-[color:var(--primary)] block w-full h-full hover:bg-[color:var(--primary)] hover:text-white" onClick={handleLogoutClick}>
+                Keluar
+              </button>
+            ) : (
+              <Link className="btn-login bg-[color:var(--secondary)] text-[color:var(--primary)] block w-full h-full hover:bg-[color:var(--primary)] hover:text-white" to="/Login" onClick={() => setIsOpen(false)}>
+                Masuk
+              </Link>
+            )}
+          </li>
+        </ul>
+      </div>
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-30">
+          <div className="bg-white rounded-lg p-6 w-80">
+            <h2 className="text-lg font-bold mb-4">Konfirmasi Logout</h2>
+            <p className="mb-4">Apakah Anda yakin ingin keluar?</p>
+            <div className="flex justify-end space-x-2">
+              <button className="px-4 py-2 bg-gray-300 rounded" onClick={handleModalClose}>
+                Batal
+              </button>
+              <button className="px-4 py-2 bg-red-500 text-white rounded" onClick={handleConfirmLogout}>
+                OK
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <Outlet />
+    </>
+  );
 };
 
 export default Header;
--- frontend/src/components/SidebarAdmin/Sidebar.jsx
+++ frontend/src/components/SidebarAdmin/Sidebar.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import Logo from "../../assets/logo.png";
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import Logo from '../../assets/logo.png';
 
 const Sidebar = (props) => {
   const navigate = useNavigate();
@@ -19,8 +19,8 @@ const Sidebar = (props) => {
   };
 
   const confirmLogout = () => {
-    localStorage.removeItem("token");
-    navigate("/Login");
+    localStorage.removeItem('token');
+    navigate('/Login');
   };
 
   const cancelLogout = () => {
@@ -33,11 +33,7 @@ const Sidebar = (props) => {
 
   return (
     <>
-      <aside
-        className={`fixed inset-y-0 left-0 z-20 w-64 bg-white border-r-2 lg:relative lg:w-auto lg:flex lg:border-none transition-transform duration-300 ${
-          isShow ? "translate-x-0" : "-translate-x-full lg:translate-x-0"
-        }`}
-      >
+      <aside className={`fixed inset-y-0 left-0 z-20 w-64 bg-white border-r-2 lg:relative lg:w-auto lg:flex lg:border-none transition-transform duration-300 ${isShow ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}`}>
         <ul className="flex flex-col justify-between h-full space-y-2 font-medium">
           <li>
             <div className="logo flex items-center mb-8 p-4">
@@ -47,52 +43,26 @@ const Sidebar = (props) => {
                 <span className="text-[color:var(--other)]">Prima</span>
               </h1>
             </div>
-            <button
-              className={`absolute top-4 right-4 shadow-none bg-white ${
-                isShow ? "block" : "hidden"
-              } lg:hidden`}
-              onClick={handleClick}
-            >
+            <button className={`absolute top-4 right-4 shadow-none bg-white ${isShow ? 'block' : 'hidden'} lg:hidden`} onClick={handleClick}>
               <i className="fal fa-times fa-xl"></i>
             </button>
           </li>
           <div className="flex-1 px-4">
             <li className="mb-4">
-              <Link
-                className={`flex items-center p-2 rounded-lg ${
-                  location.pathname === "/admin"
-                    ? "bg-blue-100 text-gray-900"
-                    : "text-gray-900 hover:bg-blue-100"
-                }`}
-                to="/admin"
-              >
+              <Link className={`flex items-center p-2 rounded-lg ${location.pathname === '/admin' ? 'bg-blue-100 text-gray-900' : 'text-gray-900 hover:bg-blue-100'}`} to="/admin">
                 <i className="far fa-user-md fa-xl"></i>
-                <span className="flex-1 ml-3 whitespace-nowrap">
-                  Manage Akun Dokter
-                </span>
+                <span className="flex-1 ml-3 whitespace-nowrap">Manage Akun Dokter</span>
               </Link>
             </li>
             <li>
-              <Link
-                className={`flex items-center p-2 rounded-lg ${
-                  location.pathname === "/admin/add"
-                    ? "bg-blue-100 text-gray-900"
-                    : "text-gray-900 hover:bg-blue-100"
-                }`}
-                to="/admin/add"
-              >
+              <Link className={`flex items-center p-2 rounded-lg ${location.pathname === '/admin/add' ? 'bg-blue-100 text-gray-900' : 'text-gray-900 hover:bg-blue-100'}`} to="/admin/add">
                 <i className="far fa-user-plus fa-xl"></i>
-                <span className="flex-1 ml-3 whitespace-nowrap">
-                  Tambah Akun Dokter
-                </span>
+                <span className="flex-1 ml-3 whitespace-nowrap">Tambah Akun Dokter</span>
               </Link>
             </li>
           </div>
           <li className="mt-auto p-4">
-            <button
-              className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-blue-100"
-              onClick={handleLogout}
-            >
+            <button className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-blue-100" onClick={handleLogout}>
               <i className="far fa-sign-out fa-2xl"></i>
               <span className="flex-1 ml-3 whitespace-nowrap">Logout</span>
             </button>
@@ -104,18 +74,13 @@ const Sidebar = (props) => {
       {showModal && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white rounded-lg p-6">
+            <h2 className="text-lg font-bold mb-4">Konfirmasi Logout</h2>
             <p>Apakah Anda yakin ingin keluar?</p>
             <div className="flex justify-end mt-4">
-              <button
-                className="mr-4 bg-gray-200 px-4 py-2 rounded-lg"
-                onClick={cancelLogout}
-              >
+              <button className="mr-4 bg-gray-200 px-4 py-2 rounded-lg" onClick={cancelLogout}>
                 Tidak
               </button>
-              <button
-                className="bg-blue-500 text-white px-4 py-2 rounded-lg"
-                onClick={confirmLogout}
-              >
+              <button className="bg-red-500 text-white px-4 py-2 rounded-lg" onClick={confirmLogout}>
                 Ya
               </button>
             </div>
