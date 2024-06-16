d168ae3159a21b5513346e5899689d489500cccf
iqbalfatoni10 <mohiqbalfatoni10@gmail.com>
2024-06-16 22:48:50 +0700

    update dashboard admin

--- frontend/src/components/HeaderAdmin/HeaderAdmin.jsx
+++ frontend/src/components/HeaderAdmin/HeaderAdmin.jsx
@@ -1,13 +1,11 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React from 'react';
 
 const HeaderAdmin = () => {
   return (
     <div>
-      <div className="">
-        <p className="text-sm pt-2 text-slate-400">Selamat Datang,</p>
-        <h1 className="text-xl pb-2">Admin</h1>
-      </div>
+      <p className="text-sm pt-2 text-slate-400">Selamat Datang,</p>
+      <h1 className="text-xl pb-2">Admin</h1>
     </div>
   );
 };
