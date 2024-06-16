dbd8359af478380a2783f7b5e37a80da293e50b3
iqbalfatoni10 <mohiqbalfatoni10@gmail.com>
2024-06-17 00:06:04 +0700

    ubah jalur file serviceworker

--- frontend/serviceworker.js
+++ frontend/serviceworker.js
@@ -1,32 +1,28 @@
 const CACHE_NAME = 'version-1';
-const urlsToChace = ['index.html', 'offline.html'];
-
-const self = this;
+const urlsToCache = ['index.html', 'offline.html'];
 
 // Install SW
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cahce');
-
-      return cache.addAll(urlsToChace);
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-// Listening for request
+// Listening for requests
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match('offline.html'));
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => caches.match('offline.html'));
     })
   );
 });
 
 // Activate the SW
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [];
-  cacheWhitelist.push(CACHE_NAME);
+  const cacheWhitelist = [CACHE_NAME];
 
   event.waitUntil(
     caches.keys().then((cacheNames) =>
